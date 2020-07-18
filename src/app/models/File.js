b572fc3d20f11a657fc1137e46f88aab65748813
Theux17 <oliveira.ms18@gmail.com>
2020-07-18 02:44:53 -0300

    Adicionado o model do File

---
+++ src/app/models/File.js
@@ -0,0 +1,63 @@
+const db = require("../../config/db")
+const fs = require("fs")
+
+
+module.exports = {
+    async create({ filename, path }) {
+        const query = `
+            INSERT INTO files(
+                name,
+                path
+            ) VALUES($1, $2)
+            RETURNING id 
+        `
+
+        const values = [
+            filename,
+            path
+        ]
+
+        return await db.query(query, values)
+    },
+
+    async update({filename, path, id}) {
+
+        const result = await db.query(`SELECT * FROM files WHERE id = $1`, [id])
+        const file = result.rows[0]
+
+        fs.unlinkSync(file.path)
+
+        const query = ` 
+        UPDATE files SET 
+            name=($1),
+            path=($2)
+            WHERE id = $3
+        `
+        const values = [
+            filename,
+            path,
+            id
+        ]
+        
+        return db.query(query, values)
+    },
+
+    async delete(id) {
+        try{
+
+            const result = await db.query(`SELECT * FROM files WHERE id = $1`, [id])
+            const file = result.rows[0]
+            
+            fs.unlinkSync(file.path)
+            
+            return db.query(`
+            DELETE FROM files WHERE id = $1
+            `, [id])
+        } catch(err){
+            console.log(err)
+        }
+
+
+    }
+
+}
